9e426510c8de6187da0862eb626a742c7ff71d80
hell25mk <hell25mk@gmail.com>
2025-02-19 23:59:16 +0900

    Add ミノを保持するクラスを追加

---
+++ Assets/Scripts/MinoQueue.cs
@@ -0,0 +1,23 @@
+using System.Collections.Generic;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+
+public class MinoQueue : MonoBehaviour{
+    private MinoGenerator minoGenerator;
+    private Queue<TetriMino> minoQueue;
+
+    public void Start() {
+        minoGenerator = GetComponent<MinoGenerator>();
+        minoQueue = new Queue<TetriMino>();
+    }
+
+    public void Refill() {
+        foreach(TetriMino mino in minoGenerator.GenerateMinoSet(transform)){
+            minoQueue.Enqueue(mino);
+        }
+    }
+
+    public TetriMino Dequeue() {
+        return minoQueue.Dequeue();
+    }
+}
