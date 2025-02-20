c5f165d6bc68b3477f6b14e790e903dc377f457d
hell25mk <hell25mk@gmail.com>
2025-02-20 21:36:50 +0900

    Add 回転機能をミノ自身に追加

--- Assets/Scripts/TetriMino.cs
+++ Assets/Scripts/TetriMino.cs
@@ -1,6 +1,11 @@
 using UnityEngine;
 
 public class TetriMino : MonoBehaviour {
+    public enum RotateDirection {
+        ClockWise,
+        CounterClockWise
+    }
+
     [SerializeField]
     private Transform[] minoChildren;
     [SerializeField]
@@ -17,9 +22,16 @@ public class TetriMino : MonoBehaviour {
         }
     }
 
-    public Vector3 MinoAxis {
-        get {
-            return minoAxis.position;
+    public void Rotate(RotateDirection direction) {
+        float angle = (direction == RotateDirection.ClockWise) ? 90.0f : -90.0f;
+        transform.RotateAround(minoAxis.position, new Vector3(0.0f, 0.0f, 1.0f), angle);
+
+        ChildrenRotateReset();
+    }
+
+    private void ChildrenRotateReset() {
+        foreach(Transform mino in minoChildren) {
+            mino.transform.rotation = Quaternion.Euler(0.0f, 0.0f, 0.0f);
         }
     }
 }
